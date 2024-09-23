a9d71e76e1f119ea44c061b940134b369ec17a6a
Chaze <chaze91@gmail.com>
2024-09-23 11:19:16 +0200

    working on csjk

--- test.py
+++ test.py
@@ -22,149 +22,110 @@ Gyerekek = [
 
 PAGE_WIDTH  = defaultPageSize[1]
 PAGE_HEIGHT = defaultPageSize[0]
-def draw_lines(canvas,doc):
-    canvas.line(50, 480, PAGE_WIDTH-50, 480)
-    canvas.line(50,480,50,50)
-    canvas.line(50, 50, PAGE_WIDTH-50, 50)
-    canvas.line(PAGE_WIDTH-50,480,PAGE_WIDTH-50,50)
+
+def cellWidth(col):
+    match col:
+            case 0: return int(200)
+            case 1: return int(200)
+            case 2: return int(80)
+            case 3: return int(80)
+            case 4: return int(100)
+            case 5: return int(20)
+            case 6: return int(20)
+            case 7: return int(100)
+            case _: return int(100)
 
 registerFont(TTFont('TNRB', 'timesbd.ttf'))
 registerFont(TTFont('TNR', 'times.ttf'))
 
-pdf_filename = "netak.pdf"
+pdf_filename = "csjk.pdf"
 c = canvas.Canvas(pdf_filename, pagesize=landscape(A4))
-
-c.line(50, 480, PAGE_WIDTH-50, 480)
-c.line(50,480,50,50)
-c.line(50, 50, PAGE_WIDTH-50, 50)
-c.line(PAGE_WIDTH-50,480,PAGE_WIDTH-50,50)
-
-header1_txt = "Adóelőleg-nyilatkozat a négy vagy több gyermeket nevelő anyák kedvezményének érvényesítéséről"
+c.setLineWidth(1.5)
+header1_txt = "Adóelőleg-nyilatkozat a családi kedvezmény és járulékkedvezmény érvényesítéséről"
 header1_txtw = stringWidth(header1_txt,fontName="TNRB",fontSize=14)
-text = c.beginText((PAGE_WIDTH - header1_txtw) / 2.0, PAGE_HEIGHT-60)
+text = c.beginText((PAGE_WIDTH - header1_txtw) / 2.0, PAGE_HEIGHT-30)
 text.setFont("TNRB", 14)
 text.textLine(header1_txt)
 c.drawText(text)
 header2_txt = f"A nyilatkozat benyújtásának éve: {datetime.datetime.now().year}"
 header2_txtw = stringWidth(header2_txt,fontName="TNR",fontSize=14)
-text = c.beginText((PAGE_WIDTH - header2_txtw) / 2.0, PAGE_HEIGHT-75)
+text = c.beginText((PAGE_WIDTH - header2_txtw) / 2.0, PAGE_HEIGHT-45)
 text.setFont("TNR", 14)
 text.textLine(header2_txt)
 c.drawText(text)
 header3_txt = "(Kérjük, kitöltés előtt olvassa el a nyilatkozathoz tartozó tájékoztatót!)"
 header3_txtw = stringWidth(header3_txt,fontName="TNRB",fontSize=14)
-text = c.beginText((PAGE_WIDTH - header3_txtw) / 2.0, PAGE_HEIGHT-90)
+text = c.beginText((PAGE_WIDTH - header3_txtw) / 2.0, PAGE_HEIGHT-60)
 text.setFont("TNRB", 14)
 text.textLine(header3_txt)
 c.drawText(text)
-text = c.beginText(60,PAGE_HEIGHT-130)
-text.setFont("TNR", 14)
-text.textLines(f"I. A nyilatkozatot adó magánszemély\nneve: Balázs Lászlóné")
+c.line(25,PAGE_HEIGHT-65,PAGE_WIDTH-25,PAGE_HEIGHT-65)
+text = c.beginText(25,PAGE_HEIGHT-80)
+text.setFont("TNR", 13)
+text.textLines(f"A nyilatkozó magánszemély\nneve: Balázs Lászlóné")
 c.drawText(text)
-text = c.beginText(300,PAGE_HEIGHT-147)
-text.setFont("TNR", 14)
+text = c.beginText(300,PAGE_HEIGHT-97)
+text.setFont("TNR", 13)
 text.textLines("Adóazonosító jele: 8359484198")
 c.drawText(text)
-text = c.beginText(625,PAGE_HEIGHT-130)
-text.setFont("TNR", 14)
-text.textLines("Módosított nyilatkozat: ⎕")
+text = c.beginText(60,PAGE_HEIGHT-125)
+text.setFont("TNR", 13)
+text.textLines(f"1. Az eltartottak adatai:")
 c.drawText(text)
-text = c.beginText(60,PAGE_HEIGHT-170)
-text.setFont("TNRB", 14)
-text.textLines("1. Gyermek neve")
+text = c.beginText(600,PAGE_HEIGHT-125)
+text.setFont("TNR", 13)
+text.textLines(f"Módosító nyilatkozat ⎕")
 c.drawText(text)
-text = c.beginText(250,PAGE_HEIGHT-170)
-text.setFont("TNRB", 14)
-text.textLines("Adóazonosító jele")
-c.drawText(text)
-text = c.beginText(400,PAGE_HEIGHT-170)
-text.setFont("TNRB", 14)
-text.textLines("Születési helye, ideje")
-c.drawText(text)
-text = c.beginText(625,PAGE_HEIGHT-170)
-text.setFont("TNRB", 14)
-text.textLines("Anyja neve")
-c.drawText(text)
-X = 190
-for gyerek in Gyerekek:
-    text = c.beginText(60,PAGE_HEIGHT-X)
-    text.setFont("TNR", 14)
-    text.textLines(gyerek.nev)
-    c.drawText(text)
-    text = c.beginText(250,PAGE_HEIGHT-X)
-    text.setFont("TNR", 14)
-    text.textLines(gyerek.ado)
-    c.drawText(text)
-    text = c.beginText(400,PAGE_HEIGHT-X)
-    text.setFont("TNR", 14)
-    text.textLines(f"{gyerek.sz_hely}, {gyerek.sz_ido}")
-    c.drawText(text)
-    text = c.beginText(625,PAGE_HEIGHT-X)
-    text.setFont("TNR", 14)
-    text.textLines(gyerek.a_nev)
-    c.drawText(text)
-    c.line(60,PAGE_HEIGHT-(X+5),785,PAGE_HEIGHT-(X+5))
-    X +=25
-text = c.beginText(60,PAGE_HEIGHT-(X+10))
-text.setFont("TNR", 14)
-text.textLines("⎕ Jelölje itt x-szel, ha a gyermekek felsorolását külön lapon folytatja!")
-c.drawText(text)
-text = c.beginText(60,PAGE_HEIGHT-(X+35))
-text.setFont("TNR", 14)
-text.textLines("2. Nyilatkozom, hogy jogosult vagyok a négy vagy több gyermeket nevelő anyák kedvezményére, azt igénybe kívánom venni. ⎕")
-c.drawText(text)
-text = c.beginText(773.75,PAGE_HEIGHT-(X+34))
-text.setFont("TNR", 14)
-text.textLines("x")
-c.drawText(text)
-text = c.beginText(60,PAGE_HEIGHT-(X+60))
-text.setFont("TNR", 14)
-text.textLines("3. Nyilatkozatomat visszavonásig kérem figyelembe venni (folytatólagos nyilatkozatot teszek) ⎕.")
-c.drawText(text)
-text = c.beginText(594.75,PAGE_HEIGHT-(X+59))
-text.setFont("TNR", 14)
-text.textLines("x")
-c.drawText(text)
-text = c.beginText(60,PAGE_HEIGHT-(X+85))
-text.setFont("TNR", 11)
-text.textLines("4. Nyilatkozom, hogy a négy vagy több gyermeket nevelő anyák kedvezményét ⎕⎕ hónaptól kezdődően nem (erre a hónapra sem) kívánom igénybe venni.")
-c.drawText(text)
-text = c.beginText(60,PAGE_HEIGHT-(X+120))
-text.setFont("TNR", 14)
-now = datetime.datetime.now()
-text.textLines(f"Kelt: {now.strftime("%Y.%m.%d")}")
-c.drawText(text)
-text = c.beginText(200,PAGE_HEIGHT-(X+120))
-text.setFont("TNR", 11)
-text.textLines("A nyilatkozatot tevő magánszemély aláírása:")
-c.drawText(text)
-c.line(400,PAGE_HEIGHT-(X+120),PAGE_WIDTH-150,PAGE_HEIGHT-(X+120))
-c.line(50,160,PAGE_WIDTH-50,160)
-text = c.beginText(60,60)
-text.setFont("TNR", 14)
-text.textLines(f"Kelt: {now.strftime("%Y.%m.%d")}")
-c.drawText(text)
-text = c.beginText(320,60)
-text.setFont("TNR", 11)
-text.textLines("cégszerű aláírás:")
-c.drawText(text)
-c.line(400,60,PAGE_WIDTH-150,60)
-text = c.beginText(60,100)
-text.setFont("TNR", 11)
-text.textLines("A nyilatkozat tartalmát tudomásul vettem. A magánszemély adóelőlegét az I. Blokkban szereplő nyilatkozat figyelembevételével állapítom meg.")
-c.drawText(text)
-text = c.beginText(60,120)
-text.setFont("TNR", 14)
-text.textLines("A munkáltató, kifizető megnevezése:")
-c.drawText(text)
-text = c.beginText(300,120)
-text.setFont("TNRB", 14)
-text.textLines("Mezőzombor Község Önkormányzata adószáma: 15726353-2-05")
-c.drawText(text)
-text = c.beginText(60,140)
-text.setFont("TNR", 14)
-text.textLines("II. Az I. Blokkban szereplő magánszemély munkáltatója/kifizetője")
-c.drawText(text)
+
+x_start = 25   # X coordinate for the start of the table
+y_start = PAGE_HEIGHT-150  # Y coordinate for the start of the table
+cell_width = 100  # Width of each cell
+cell_heights = 20  # Height of each cell
+num_rows = 4      # Number of rows
+num_cols = 8      # Number of columns
+
+data = [
+    ["Név", "Adóazonosító", "TAJ szám", "Anyja neve","Cím","EM**","JJ**","Változás időpontja"],
+    ["Balogh Dom","8526611372","130-710-666","Rontó Izabella","Kinizsi u. 13.",'1',"a",""],
+    ["Balogh Dok","8526611372","130-710-666","Rontó Izabella","Kinizsi u. 13.",'1',"a",""],
+    ["Balogh Dom","8526611372","130-710-666","Rontó Izabella","Kinizsi u. 13.",'1',"a",""],
+]
+
+
+def findLongest(col):
+    longest = 0
+    for row_idx, row in enumerate(data):
+         current = stringWidth(data[row_idx][col],"TNR",12)
+         if current > longest :
+              longest = current
+    return longest          
+cell_widths = [findLongest(0)+20,findLongest(1)+20,findLongest(2)+20,findLongest(3)+20,findLongest(4)+20,30,30,100] 
+for row in range(num_rows + 1):  # +1 to draw the last horizontal line
+    y = y_start - row * cell_heights
+    c.line(x_start, y, x_start + sum(cell_widths), y)  # Draw horizontal lines
+
+# Draw vertical lines with different column widths
+current_x = x_start
+for col_width in cell_widths:
+    c.line(current_x, y_start, current_x, y_start - num_rows * cell_heights)
+    current_x += col_width
+
+# Draw the last vertical line at the end of the last column
+c.line(current_x, y_start, current_x, y_start - num_rows * cell_heights)
+
+
+c.setFont("TNR",12)
+for row_idx, row in enumerate(data):
+    current_x = x_start  # Reset x position for each row
+    for col_idx, cell in enumerate(row):
+        # Calculate text position within the cell
+        x = current_x + 5  # Add some padding from the left
+        y = y_start - row_idx * cell_heights - 15  # Adjust vertical position for text
+        c.drawString(x, y, cell)
+        
+        # Move to the next column by adding the width of the current cell
+        current_x += cell_widths[col_idx]
+
 
 c.save()
 print(f"PDF created: {pdf_filename}")
